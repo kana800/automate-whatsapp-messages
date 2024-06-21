ce05244a4a7ee4ff31df5961463887b909e63b54
kana880 <64830745+kana800@users.noreply.github.com>
2024-06-21 13:52:57 +0530

    added test scripts

---
+++ test/client.py
@@ -0,0 +1,12 @@
+from multiprocessing.connection import Client
+
+if __name__ == "__main__":
+    address = ('localhost', 6000)
+    conn = Client(address, authkey=b'secretkey')
+
+    while True:
+        raw_input = input("type message: ")
+        conn.send(raw_input)
+        if raw_input == "close":
+            break
+    conn.close()
