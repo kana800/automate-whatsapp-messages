8bf23b3f440241a2faf991dbe3e0cf77788d2d8d
kanna <64830745+kana800@users.noreply.github.com>
2024-09-05 18:31:42 +0530

    reverted back to sockets
    reverted back to sockets

--- automater/automater.py
+++ automater/automater.py
@@ -13,18 +13,36 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
+from multiprocessing.connection import Listener
 
 # contact name is You by default
 # will send a message to yourself
 contact_name = "You"
 
+def getmessageinformation(msg):
+    """
+    summary: separates the message
+    and the sender and return in a 
+    tuple
+    """
+    msg = msg.lstrip().rstrip()
+    if msg[:4] != "send":
+        print("send keyword isn't found\nusage: send <message> to:<name>")
+        return (None, "You")
+    content = msg[4:].lstrip().rstrip()
+    to_index = content.rfind("to:") 
+    if to_index == -1:
+        print("to keyword isn't found\nusage: send <message> to:<name>")
+        return (None, "You")
+    contact_name = content[to_index:].split('to:')[1].lstrip().rstrip()
+    return (content[:to_index].rstrip(), contact_name)
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-headless", type=bool, default=False, help="run automator headless")
-    parser.add_argument("contactname",type=str, default="You", help="contact name (example:'sam smith')")
-    parser.add_argument("message",type=str, default="this is a test message", help="message ( example:'this is a test message'")
     args = parser.parse_args()
 
     # check the docs on how to bypass the whatsapp web login everytime
@@ -34,11 +52,17 @@ if __name__ == "__main__":
     options = FirefoxOptions()
     if args.headless: 
         options.add_argument("--headless")
-    firefoxprofile = webdriver.FirefoxProfile()
+    firefoxprofile = webdriver.FirefoxProfile("")
     options.profile = firefoxprofile 
     driver = webdriver.Firefox(options=options)
     driver.get("https://web.whatsapp.com/")
-    WebDriverWait(driver, 5200)
+    wait = WebDriverWait(driver, 5200)
+    wait.until(EC.visibility_of_element_located((By.XPATH,"/html/body/div[1]/div/div/div[2]/div[3]/header/header/div/div[1]/h1")))
+    # activating the socket connection 
+    address = ('localhost', 6000)
+    # add the secret key
+    listener = Listener(address, authkey=b'secretkey')
+    conn = listener.accept()
 
     # everything from this point onwards is the
     # clicking and typing;
@@ -49,26 +73,33 @@ if __name__ == "__main__":
     # 7. press ENTER
     # 8. press ESC
     actions = ActionChains(driver)
-    # format: send-message <contact-name> <message>
-    # TODO: add client feedback here; send message back
-    # sending the message failed
-    contact_name = args.contactname
-    message = args.message
-    print(f"contact name: {contact_name} | message: {message}")
-    if message and contact_name:
-        # CTRL + ALT + N
-        actions.key_down(Keys.CONTROL).key_down(Keys.ALT).send_keys('n').key_up(Keys.ALT).key_up(Keys.CONTROL).perform()
-        actions.send_keys(contact_name).perform()
-        body = driver.find_element(By.TAG_NAME, 'body')
-        # TODO: add error checking here
-        if (body.text.find("No results found")) == -1:
-            actions.send_keys(Keys.ENTER).perform()
-            actions.send_keys(message)
-            actions.send_keys(Keys.ENTER).perform()
-            actions.send_keys(Keys.ESCAPE).perform()
+    while True:
+        # format: send-message <contact-name> <message>
+        msg = conn.recv()
+        if msg == 'close':
+            conn.close()
+            break
+        # TODO: add client feedback here; send message back
+        # sending the message failed
+        (message, contact_name) = getmessageinformation(msg)
+        print(f"contact name: {contact_name} | message: {message}")
+        if message and contact_name:
+            # CTRL + ALT + N
+            actions.key_down(Keys.CONTROL).key_down(Keys.ALT).send_keys('n').key_up(Keys.ALT).key_up(Keys.CONTROL).perform()
+            actions.send_keys(contact_name).perform()
+            body = driver.find_element(By.TAG_NAME, 'body')
+            # TODO: add error checking here
+            if (body.text.find("No results found")) == -1:
+                actions.send_keys(Keys.ENTER).perform()
+                actions.send_keys(message)
+                actions.send_keys(Keys.ENTER).perform()
+                actions.send_keys(Keys.ESCAPE).perform()
+            else:
+                print("contact name cannot be found")
+                actions.send_keys(Keys.ESCAPE).perform()
+                continue
         else:
-            print("contact name cannot be found")
-            actions.send_keys(Keys.ESCAPE).perform()
-    else:
-        print("request cannot be processed")
+            print("request cannot be processed")
+            continue
+    listener.close()
     driver.quit()
